1d7a40735aa689658ac6030cc3006a932e2dcbab
Eugene Arzamastsev <earzamastsev@gmail.com>
2020-12-03 12:52:33 +0500

    Finish 2-module-1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let salary in salaries) {
+    if (+salaries[salary]) {
+      result += salaries[salary];
+    } 
+  }
+  return result;
 }
