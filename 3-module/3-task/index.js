25167d18cc7519a6628177ea1e6a487f4d3c548c
Eugene Arzamastsev <earzamastsev@gmail.com>
2020-12-03 15:39:32 +0500

    Finish 3-module-3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,15 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  let tmpArray = str.split("-");
+  result = [];
+  for (let word of tmpArray) {
+    if ( word == "" ) continue;
+    if (tmpArray.indexOf(word) == 0) {
+      result.push(word)
+    } else {
+    result.push(word[0].toUpperCase() + word.slice(1))
+    }
+  }
+  return result.join("")
 }
