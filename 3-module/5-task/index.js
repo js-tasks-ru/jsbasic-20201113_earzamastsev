29067381a402669b2ef146593ef81cb6f92091a6
Eugene Arzamastsev <earzamastsev@gmail.com>
2020-12-03 22:24:52 +0500

    Finish 3-module-5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,12 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let re = /[\d-.]+/g;
+  let numbersArray = str.match(re);
+  numbersArray.sort((a, b) => a - b);
+  let result = {
+    min: +numbersArray[0],
+    max: +numbersArray[numbersArray.length-1]
+  };
+  return result;
 }
