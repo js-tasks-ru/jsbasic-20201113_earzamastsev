e462ed76c69f1eb13b2dbe93645bf62d2c590654
Eugene Arzamastsev <earzamastsev@gmail.com>
2020-12-09 14:53:14 +0500

    Finish 5-module-2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,5 @@
 function toggleText() {
-  // ваш код...
+  let buttonElem = document.querySelector('.toggle-text-button');
+  let textElem = document.querySelector('#text');
+  buttonElem.addEventListener('click', () => {textElem.toggleAttribute('hidden')} );
 }
