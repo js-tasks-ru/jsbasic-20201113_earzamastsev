b8cc504c76fd3afb24c7c5b6cfcec1f8940e7811
Eugene Arzamastsev <earzamastsev@gmail.com>
2020-12-15 16:56:19 +0500

    Finish 6-module-1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -28,7 +28,51 @@
  *
  * @constructor
  */
+
 export default class UserTable {
   constructor(rows) {
+    this.table = document.createElement('table');
+    const HeaderTemplate = `<thead>
+    <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+    </tr>
+</thead>`;
+    this.table.insertAdjacentHTML('afterBegin', HeaderTemplate);
+    this.data = rows;
+    this.table.addEventListener('click', this.remove);
+    this.elem = this.render();
   }
+
+ render()  {
+  if (!this.tbody) {
+    this.tbody = document.createElement('tbody');
+  }
+
+  for (let row of this.data) {
+    let template = `<tr>
+          <td>${row.name}</td>
+          <td>${row.age}</td>
+          <td>${row.salary}</td>
+          <td>${row.city}</td>
+          <td><button class='delete'>X</button></td>
+        </tr>`;
+    this.tbody.insertAdjacentHTML('beforeEnd', template);
+  }
+    this.table.append(this.tbody);
+    return this.table;
+
+ }  
+
+remove(event) {
+  let target = event.target;
+    if (target.tagName == 'BUTTON') {
+        event.target.closest("tr").remove();
+    }
 }
+}
+
+
